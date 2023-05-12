a7846987ee5ec8ddb90bc1eb7fcc0b8da32504d5
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-05-12 12:11:50 -0400

    modulo formato orden de ventas y compras v1

---
+++ Ex/ex_formato_orden_venta_compra/formatos/formato.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from odoo.tools.misc import DEFAULT_SERVER_DATE_FORMAT
+
+from odoo import models, fields, api, _, tools
+from odoo.exceptions import UserError
+import openerp.addons.decimal_precision as dp
+import logging
+
+import io
+from io import BytesIO
+
+import xlsxwriter
+import shutil
+import base64
+import csv
+import xlwt
+import xml.etree.ElementTree as ET
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+
+    def float_format(self,valor):
+        #valor=self.base_tax
+        if valor:
+            result = '{:,.2f}'.format(valor)
+            result = result.replace(',','*')
+            result = result.replace('.',',')
+            result = result.replace('*','.')
+        else:
+            result = "0,00"
+        return result
