51a9bea73fd3185a440a36db1dea135c45ab9494
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-05-22 00:09:54 -0400

    ajuste de formato orden de venta

---
+++ Ex/ex_formato_orden_venta_compra/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Formatos particular pedidos de ventas y compras",
+
+    'summary': """Formatos particular pedidos de ventas y compras""",
+
+    'description': """
+       Formatos particular pedidos de ventas y compras.
+    """,
+    'version': '13.0',
+    'author': 'INM & LDR Soluciones Tecnológicas y Empresariales C.A',
+    'category': 'Tools',
+    'website': 'http://soluciones-tecno.com/',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account','purchase','sale'],
+
+    # always loaded
+    'data': [
+        'formatos/sale_orden.xml',
+    ],
+    'application': True,
+    'active':False,
+    'auto_install': False,
+}
